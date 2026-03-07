7c5d16bb2ff1eb77a12595e65c9d8a5299386a15
LAB13_USER ON WS18 <L13Y12W18@LabHCI.net>
2026-03-07 11:55:00 +0800

    Initial commit

---
+++ boardinghouse/src/test/java/edu/cit/ferrater/boardinghouse/BoardinghouseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.ferrater.boardinghouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardinghouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
